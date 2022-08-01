616b0675c115942d6269ad024d8c28b7bbc8ffca
murad <mhdite7@gmail.com>
2022-08-01 20:15:58 +0300

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesTableSeeder::class,
             UsersTableSeeder::class,
+            BehaviorsTableSeeder::class,
         ]);
     }
 }
